939b35c56c49983076ab5ab3413d696010cdf5d9
Burhan <burhan.umt@gmail.com>
2025-09-18 16:13:10 +0300

    added project setup

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Hellow i am Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
